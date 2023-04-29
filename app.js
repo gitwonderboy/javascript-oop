eaff37dcb92c27bf6c010aeacb4f0fc4429c57b4
Wonderboy Mthiyane <codelad1@gmail.com>
2023-04-29 17:08:55 +0200

    More detailing and examples
    Signed-off-by: Wonderboy Mthiyane <codelad1@gmail.com>

--- app.js
+++ app.js
@@ -14,10 +14,28 @@ let Phone = function (brand, androidVersion, price, year) {
 
   this.calcModel = function () {
     let model = new Date().getFullYear() - this.year;
-    console.log(model);
+    console.log(`This Smart Phone was launched ${model} years ago.`);
   };
 };
 
-let oppo = new Phone("OPPO", "V10", 23000, 2022);
-oppo.calcModel();
+// Why do we use The new Operator
+/**
+ * The new Operator does three things while creating an object using function constructor
+ *
+ * The new Operator first creates an empty object on function constructor
+ * It also make sure that the 'this' variable in function constructor point to the newly created empty object
+ * Finally, it returns the object from the function constructor
+ */
+
+let oppo = new Phone("OPPO", "V10", 23000, 2020);
 console.log(oppo);
+oppo.calcModel();
+
+// If you were to create an Array Object like:
+numbers = [10, 20, 5, 5, 4]
+console.log(numbers[0])
+
+/**
+ * The above Array is created from new Array() function constructor.
+ * Inherit all methods / functions that new Array() constructor has.
+ */
